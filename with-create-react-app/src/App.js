b15f3c4e1403bc11bb38ffce74f691d12c96ce7f
Konstantin Grushetsky <github@grushetsky.net>
2019-04-03 13:53:32 +0300

    Test

--- with-create-react-app/src/App.js
+++ with-create-react-app/src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const b = 5;
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p>Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
